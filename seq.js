bbd61bf8002bc443a194be881989ecd3d480d797
ChidiOmen <ochidimon@utexas.edu>
2016-05-11 19:00:38 -0500

    Time key presses

--- seq.js
+++ seq.js
@@ -1,1 +1,17 @@
 console.log('Hello, World!');
+var timeStamps = {};
+
+window.onkeydown = function(e) {
+	if (!timeStamps[e.which]) {
+		timeStamps[e.which] = +new Date();
+	}
+};
+
+window.onkeyup = function(e){
+	currTime = +new Date();
+	if (timeStamps[e.which]) {
+		duration = currTime - timeStamps[e.which];
+		console.log('' + e.which + ': ' + duration);
+		delete timeStamps[e.which];
+	}
+};
