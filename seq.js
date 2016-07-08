5ae621f0adfde0ffeabbafd8eb15c86b5544a03b
ChidiOmen <ochidimon@utexas.edu>
2016-07-08 16:41:26 -0500

    Pushing dev branch

--- seq.js
+++ seq.js
@@ -1,13 +1,31 @@
-console.log('Hello, World!');
 var timeStamps = {};
+var i = 0;
+var interval = null;
+
+sayHello = function() {
+	console.log('' + i + ' seconds');
+	i++;
+	if (i > 200) {
+		clearTimeout(interval);
+	}
+}
+
+var interval = window.setInterval(sayHello, 30);
+console.log('Goodbye.');
 
 window.onkeydown = function(e) {
+	// When key is pressed down, ad to list of currently
+	// pressed keys
 	if (!timeStamps[e.which]) {
-		timeStamps[e.which] = +new Date();
+		var num = +new Date();
+		timeStamps[e.which] = num;
+		document.getElementById("text").innerHTML = num;
 	}
 };
 
 window.onkeyup = function(e){
+	// When key is up, remove from list of currently
+	// pressed keys
 	currTime = +new Date();
 	if (timeStamps[e.which]) {
 		duration = currTime - timeStamps[e.which];
@@ -15,3 +33,4 @@ window.onkeyup = function(e){
 		delete timeStamps[e.which];
 	}
 };
+
