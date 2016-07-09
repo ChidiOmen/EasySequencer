d46cc98a7d888f1af6b87722be5660e2c41bb276
ChidiOmen <ochidimon@utexas.edu>
2016-07-09 13:54:46 -0500

    Added visual representation of notes

--- seq.js
+++ seq.js
@@ -1,3 +1,4 @@
+// {keycode: {start, elem}}
 var timeStamps = {};
 var times = [];
 var i = 0;
@@ -11,12 +12,21 @@ sayHello = function() {
 	}
 }
 
+styleDiv = function(div){
+	div.style.background = "green";
+	div.style.height = "20px";
+	div.style.border = "2px solid black";
+}
+
 
 eval = function() {
 	str = ""
 	for (var key in timeStamps) {
-		timeStamps[key] = +new Date();
-		str += key + " " + timeStamps[key] + '\n'
+		var duration = +new Date() - timeStamps[key].start;
+		var elem = timeStamps[key].elem;
+		elem.style.width = duration + "px";
+		document.getElementById('notes').appendChild(elem);
+		str += key + ": " + duration + "\n"
 	}
 	i++;
 	document.getElementById("text").innerHTML = str;
@@ -27,7 +37,11 @@ window.onkeydown = function(e) {
 	// pressed keys
 	if (!timeStamps[e.which]) {
 		var num = +new Date();
-		timeStamps[e.which] = num;
+		timeStamps[e.which] = {};
+		timeStamps[e.which].start = num;
+		timeStamps[e.which].elem = document.createElement("div");
+		styleDiv(timeStamps[e.which].elem);
+		timeStamps[e.which].elem.scrollIntoView();
 	}
 };
 
